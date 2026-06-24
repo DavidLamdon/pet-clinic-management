6db8e939a7a2ebcdeaa8f44b069aac8adadf14de
DavidLamdon <vladag770@gmail.com>
2026-06-24 11:15:37 +0300

    First steps set up Tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className="text-3xl font-bold text-blue-600">
       <main className={styles.main}>
         <Image
           className={styles.logo}
